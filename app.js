e11dd0b2e1675f3e425bb3ead0973d5042524049
Tim <timmlaxton@gmail.com>
2019-11-06 09:33:20 +0000

    added UI class

---
+++ app.js
@@ -0,0 +1,16 @@
+class UI {
+  constructor() {
+    this.budgetFeedback = document.querySelector(".budget-feedback");
+    this.expenseFeedback = document.querySelector(".expense-feeback");
+    this.budgetForm = document.getElementById("budget-form");
+    this.budgetInput = document.getElementById("budget-input");
+    this.budgetAmount = document.getElementById("expense-amount");
+    this.balance = document.getElementById("balance");
+    this.balanceAmount = document.getElementById("balance-amount");
+    this.expenseForm = document.getElementById("expense-form");
+    this.expsenseInput = document.getElementById("expense-input");
+    this.expenseList = document.getElementById("expense-list");
+    this.itemList - [];
+    this.itemID = 0;
+  }
+}
