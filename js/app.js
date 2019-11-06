01846c6f8ebca0c0e00d23e7714fe0af16ff0b19
Tim <timmlaxton@gmail.com>
2019-11-06 10:39:42 +0000

    added event listeners

--- js/app.js
+++ js/app.js
@@ -1,18 +1,48 @@
 class UI {
   constructor() {
+
     this.budgetFeedback = document.querySelector(".budget-feedback");
-    this.expenseFeedback = document.querySelector(".expense-feedback");
+    this.expenseFeedback = document.querySelector(".expense-feeback");
     this.budgetForm = document.getElementById("budget-form");
     this.budgetInput = document.getElementById("budget-input");
+    this.budgetAmount = document.getElementById("expense-amount");
     this.budgetAmount = document.getElementById("budget-amount");
     this.expenseAmount = document.getElementById("expense-amount");
     this.balance = document.getElementById("balance");
     this.balanceAmount = document.getElementById("balance-amount");
     this.expenseForm = document.getElementById("expense-form");
-    this.expenseInput = document.getElementById("expense-input");
-    this.amountInput = document.getElementById("amount-input");
+    this.expsenseInput = document.getElementById("expense-input");
     this.expenseList = document.getElementById("expense-list");
-    this.itemList = [];
+    this.itemList - [];
     this.itemID = 0;
   }
+submitBudgetForm(){
+  console.log("Hiya");
 }
+}
+
+function eventListeners(){
+  const budgetForm = document.getElementById('budget-form');
+  const expenseForm = document.getElementById('expense-form');
+  const expenseList = document.getElementById('expense-list');
+
+  const ui = new UI()
+
+
+budgetForm.addEventListener('submit', function(event){
+event.preventDefault();
+ui.submitBudgetForm();
+})
+
+expenseForm.addEventListener('submit', function(event){
+event.preventDefault();
+})
+
+expenseList.addEventListener('click', function(event){
+event.preventDefault();
+})
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+  eventListeners();
+})
